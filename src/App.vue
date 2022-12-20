94e0228619f7fe6b1e4608d6280e19a38e3551e5
RunyShark <spartadragon117@live.com.mx>
2022-12-20 08:18:39 -0600

    FUNC(Counter)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Counter />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import Counter from './components/Counter.vue';
 
 @Options({
+  name: 'app',
   components: {
-    HelloWorld,
+    Counter,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Counert</h2>
+  <p>{{ this.counter }}</p>
+  <button @click="sumar">Sumar + 1</button>
+  <button @click="restar">Restar - 1</button>
+  <button @click="reiniciar">Reiniciar</button>
+</template>
+
+<script>
+export default {
+  name: 'contador',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    sumar() {
+      this.counter += 1;
+    },
+    restar() {
+      this.counter -= 1;
+    },
+    reiniciar() {
+      this.counter = 0;
+    },
+  },
+};
+</script>
+
+<style></style>
